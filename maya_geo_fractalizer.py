cd5e12ac43929de2fe29ffff20c0c0ab2c1bcd19
maxvanschendel <43640596+maxvanschendel@users.noreply.github.com>
2019-09-26 04:29:26 +0200

    Update maya_geo_fractalizer.py

--- maya_geo_fractalizer.py
+++ maya_geo_fractalizer.py
@@ -6,24 +6,25 @@ def copy2face(object, depth, max_depth, scale_factor):
         cmds.select(object)
         num_faces = cmds.polyEvaluate(f=True)
         
-        for f in range(num_faces):
+        # place instances of self on own faces
+        for f in range(num_faces):           
+            # calculate face centroid
+            coords_sms = [0, 0, 0]
             face = cmds.select(object + '.f[%i]' % f)
             vertices = cmds.ls(cmds.polyListComponentConversion(toVertex=True), flatten=True)
-        
-            coords_sms = [0, 0, 0]
+            
             for v in vertices:
                 cmds.select(v)
-                pos = cmds.pointPosition()
-            
+                pos = cmds.pointPosition()            
                 for i in range(len(coords_sms)):
-                    coords_sms[i] += pos[i]
-        
+                    coords_sms[i] += pos[i]   
+                    
             face_center = [c/len(vertices) for c in coords_sms]          
             
             new_obj=cmds.instance(object)
             cmds.select(new_obj)
-            cmds.move(face_center[0],face_center[1],face_center[2])
-            cmds.scale(scale_factor,scale_factor,scale_factor,r=True)
+            cmds.move(face_center[0], face_center[1], face_center[2])
+            cmds.scale(scale_factor, scale_factor, scale_factor, r=True)
 
             copy2face(new_obj[0], depth+1, max_depth, scale_factor)
 
