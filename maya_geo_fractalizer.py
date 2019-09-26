11396530c8803add6d14d4acb49a0b0cbf94691d
maxvanschendel <43640596+maxvanschendel@users.noreply.github.com>
2019-09-26 21:34:05 +0200

    Update maya_geo_fractalizer.py

--- maya_geo_fractalizer.py
+++ maya_geo_fractalizer.py
@@ -1,41 +1,46 @@
 import maya.cmds as cmds
 
+def instance_at_point(object, pos, scale_factor):
+    new_obj=cmds.instance(object, lf=True)
+    cmds.select(new_obj)
+    cmds.move(pos[0], pos[1], pos[2])
+    cmds.scale(scale_factor, scale_factor, scale_factor, r=True)
+    
+    return new_obj
+
 # recursively copy geometry onto its own faces, changing scale with every step
-def copy2face(object, depth, max_depth, scale_factor):
+def recursive_instancer(object, depth, max_depth, scale_factor, mode):
     if depth < max_depth:        
         cmds.select(object)
-        num_faces = cmds.polyEvaluate(f=True)
         
-        # place instances of self on own faces
-        for f in range(num_faces):           
-            # calculate face centroid
-            coords_sms = [0, 0, 0]
-            face = cmds.select(object + '.f[%i]' % f)
+        if mode == 'centroid':
+            num_faces = cmds.polyEvaluate(f=True)
+            
+            for f in range(num_faces):           
+                coords_sms = [0, 0, 0]
+                face = cmds.select(object + '.f[%i]' % f)
+                vertices = cmds.ls(cmds.polyListComponentConversion(toVertex=True), flatten=True)
+                
+                for v in vertices:
+                    cmds.select(v)
+                    pos = cmds.pointPosition()            
+                    for i in range(len(coords_sms)):
+                        coords_sms[i] += pos[i]   
+                        
+                face_center = [c/len(vertices) for c in coords_sms]
+                new_obj = instance_at_point(object, face_center, scale_factor)   
+                recursive_instancer(new_obj[0], depth+1, max_depth, scale_factor, mode=mode)
+                
+        elif mode == 'vertex':
             vertices = cmds.ls(cmds.polyListComponentConversion(toVertex=True), flatten=True)
-            
             for v in vertices:
-                cmds.select(v)
-                pos = cmds.pointPosition()            
-                for i in range(len(coords_sms)):
-                    coords_sms[i] += pos[i]   
+                new_obj = instance_at_point(object, cmds.pointPosition(v), scale_factor)
+                recursive_instancer(new_obj[0], depth+1, max_depth, scale_factor, mode=mode)
                     
-            face_center = [c/len(vertices) for c in coords_sms]          
-            
-            new_obj=cmds.instance(object)
-            cmds.select(new_obj)
-            cmds.move(face_center[0], face_center[1], face_center[2])
-            cmds.scale(scale_factor, scale_factor, scale_factor, r=True)
-
-            copy2face(new_obj[0], depth+1, max_depth, scale_factor)
 
 # feed selected object into recursive function
 init_object = cmds.ls(sl=True, long=True)
-copy2face(object=init_object[0], depth=0, max_depth=3, scale_factor=0.33)
-
-
-    
-
-
-        
-     
-
+if init_object:
+    recursive_instancer(object=init_object[0], depth=0, max_depth=4, scale_factor=0.5, mode = 'vertex')
+else:
+    print('Error: No initial object selected')
