9888379ba19821c33baec5228610b0bccd2a42da
maxvanschendel <43640596+maxvanschendel@users.noreply.github.com>
2019-10-08 20:25:47 +0200

    Update houdini_geo_fractalizer.py

--- houdini_geo_fractalizer.py
+++ houdini_geo_fractalizer.py
@@ -9,44 +9,72 @@ def translate_obj(obj, p):
     
     
 def scale_obj(obj, scale, rand_scale):
+    print(rand_scale)
     obj.parm('sizex').set(obj.parm('sizex').eval() * (scale[0] + uniform(-rand_scale[0], rand_scale[0])))
     obj.parm('sizey').set(obj.parm('sizey').eval() * (scale[1] + uniform(-rand_scale[1], rand_scale[1])))
     obj.parm('sizez').set(obj.parm('sizez').eval() * (scale[2] + uniform(-rand_scale[2], rand_scale[2])))
     
   
-def rotate_obj(obj, rand_rot, rot):
+def rotate_obj(obj, rot, rand_rot):
     obj.parm('rx').set(rot[0] + uniform(-rand_rot[0], rand_rot[0]))
     obj.parm('ry').set(rot[1] + uniform(-rand_rot[1], rand_rot[1]))
     obj.parm('rz').set(rot[2] + uniform(-rand_rot[2], rand_rot[2]))
 
     
+def polygon_centroid(poly):
+    points = [i.position() for i in poly.points()]
+    
+    sum = [0, 0, 0]
+    
+    for p in points:
+        sum[0] += p[0]
+        sum[1] += p[1]
+        sum[2] += p[2]
+        
+    return [sum[0]/len(points), sum[1]/len(points), sum[2]/len(points)]    
+    
+    
 def instance_at_point(obj, p, scale, rotate, rand_scale, rand_rot):
     sub_obj=n.copyItems([obj])[0]
 
     translate_obj(sub_obj, p.attribValue('P'))
-    scale_obj(sub_obj, rand_scale, scale)
-    rotate_obj(sub_obj, rand_rot, rotate)
+    scale_obj(sub_obj, scale, rand_scale)
+    rotate_obj(sub_obj, rotate, rand_rot)
     
     return sub_obj
     
     
-def recursive_instancer(obj, iterations, scale, rotate):
-    if iterations > 0:
-        points = obj.geometry().points()
+def recursive_instancer(obj, iterations, mode, scale, rotate, mode_switching):
+    if mode_switching == True:
+        modes = ['centroid', 'vertex']
+        mode = modes[randint(0, len(modes) - 1)]
         
-        for p in points:
-        
-            sub_obj = instance_at_point(obj, p, scale, rotate, rand_scale, rand_rot)
-            recursive_instancer(sub_obj, iterations - 1, scale, rotate)
+    if iterations > 0:   
+        if mode == 'vertex':
+            points = obj.geometry().points()
             
+           
+        elif mode == 'centroid':
+            faces = obj.geometry().prims()
+            points = []
+            
+            for f in faces:
+                point = hou.Geometry().createPoint()
+                point.setPosition(polygon_centroid(f))
+                points.append(point)
 
-    
+                
+        for p in points:            
+            sub_obj = instance_at_point(obj, p, scale, rotate, rand_scale, rand_rot)
+            recursive_instancer(sub_obj, iterations - 1, mode, scale, rotate, mode_switching)
+            
+                             
 n = hou.node('obj').createNode('geo')
 box = n.createNode('box')
 
 scale_factor = (0.5, 0.5, 0.5)
 rot_factor = (0, 0, 0)
-rand_scale = (0.5, 0.5, 0.5)
-rand_rot = (45, 0, 0)
+rand_scale = (0, 0, 0)
+rand_rot = (0, 0, 0)
     
-recursive_instancer(box, 2, scale_factor, rot_factor) 
+recursive_instancer(box, 2, 'centroid', scale_factor, rot_factor, True) 
